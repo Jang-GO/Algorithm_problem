29eb502d9eb053b6ec78765babc347ddd395ebda
Jang-GO <88973153+Jang-GO@users.noreply.github.com>
2022-07-11 18:21:05 +0900

    Create prob4.py

---
+++ Section2/prob4/prob4.py
@@ -0,0 +1,29 @@
+''' 
+1.n명의 학생의 평균을 구하고
+2. n명의 학생중 평균애 가까운 학생은 몇번쨰 학생인지
+(여러명 우선순위: 1.점수가 높은학생 2.학생번호가 빠른 학생의 번호)
+'''
+import sys
+sys.stdin = open("input.txt",'rt')
+
+n = int(input())
+k = list(map(int,input().split()))
+
+total = 0
+average = 0
+for i in k:
+    total +=i
+average = round(total / n)
+
+score = 0
+min = k[0]
+for idx,x in enumerate(k):
+    if abs(x-average) < min:
+        min = abs(x-average)
+        score = x
+        res = idx +1
+    elif abs(x-average)==min:
+        if x>score: 
+            score = x
+            res = idx+1
+print(average,res)
